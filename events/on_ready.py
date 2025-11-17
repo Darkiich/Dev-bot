b7dbca14eef3a31eeb97aec1b2728d93c7c851cb
Darki255 <Kurachiy75@gmail.com>
2025-11-17 20:13:38 +0300

    Добавил запуск задачи на обновление списка команды

--- events/on_ready.py
+++ events/on_ready.py
@@ -1,6 +1,7 @@
 from bot_init import bot
 from tasks.discord_auth import RegisterButton, discord_auth_update
 from tasks.team_list import list_team_task
+from tasks.status_message import status_update
 
 
 @bot.event
@@ -8,3 +9,4 @@ async def on_ready():
     bot.add_view(RegisterButton())
     discord_auth_update.start()
     list_team_task.start()
+    status_update.start()
