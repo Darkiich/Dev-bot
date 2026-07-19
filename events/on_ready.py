4f4c5aff6793193e450f1fd304876c92b0378922
Darkiich <ne>
2026-07-19 08:09:45 +0300

    init

--- events/on_ready.py
+++ events/on_ready.py
@@ -2,6 +2,7 @@ from bot_init import bot
 from tasks.discord_auth import RegisterButton, discord_auth_update
 from tasks.team_list import list_team_task
 from tasks.status_message import status_update
+from tasks.sponsor_role_sync import sponsor_role_sync
 
 
 @bot.event
@@ -10,3 +11,4 @@ async def on_ready():
     discord_auth_update.start()
     list_team_task.start()
     status_update.start()
+    sponsor_role_sync.start()
